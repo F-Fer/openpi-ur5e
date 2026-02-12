203fdc4db5e4b9c53402340655a730f26ebe4b51
Finn <ffgm2004@gmail.com>
2026-02-12 09:09:48 +0100

    Add r128 lora config

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1302,7 +1302,35 @@ _CONFIGS = [
             paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
         ).get_freeze_filter(),
         ema_decay=None,
-        keep_period=5_000,
+        keep_period=10_000,
+        batch_size=24,
+    ),
+    TrainConfig(
+        name="pi0_ur_tasks_merged_lora_r128",
+        model=pi0_config.Pi0Config(
+            paligemma_variant="gemma_2b_lora",
+            action_expert_variant="gemma_300m_lora",
+            paligemma_lora_rank=16, # 16 is the default rank
+            paligemma_lora_alpha=16, # 16 is the default alpha
+            action_expert_lora_rank=128,
+            action_expert_lora_alpha=128,
+            action_horizon=30,
+        ),
+        data=LeRobotUR5DataConfig(
+            repo_id="F-Fer/ur-task1",
+            base_config=DataConfig(
+                prompt_from_task=True,
+            ),
+            assets=AssetsConfig(asset_id="F-Fer/ur-tasks-merged"),
+            extra_delta_transform=True,
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_base/params"),
+        num_train_steps=60_000,
+        freeze_filter=pi0_config.Pi0Config(
+            paligemma_variant="gemma_2b_lora", action_expert_variant="gemma_300m_lora"
+        ).get_freeze_filter(),
+        ema_decay=None,
+        keep_period=10_000,
         batch_size=24,
     )
 ]
